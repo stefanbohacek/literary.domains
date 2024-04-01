7b67375a44d5bb6cd773a959ea6a7f1a1cf41974
Stefan Bohacek <stefan@stefanbohacek.com>
2024-04-01 10:02:41 -0400

    Debugging site performance.

--- blueprints/generate_domain_names.py
+++ blueprints/generate_domain_names.py
@@ -19,12 +19,12 @@ def generate_domain_names():
     if not domains_file.is_file():
         book_text_file = Path("/path/to/file")
         if not book_text_file.is_file():
-            # print("downloading book file...")
+            print("downloading book file...")
             with open(book_text_file_path, "w") as book_text_file:
                 if (book_text):
                     book_text_file.write(book_text)
 
-        # print("generating hosts...")
+        print("generating hosts...")
         hosts = public_domains.get_hosts(book_text_file_path, quiet=True)
 
         with open(domains_file_path, "w") as domains_file:
